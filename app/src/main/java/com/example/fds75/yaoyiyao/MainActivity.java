1e1c35cb37e13d754d2321238b838bfd81f77fd6
luxxxian <wyh789456@163.com>
2019-06-12 19:56:33 +0800

    剪刀石头布——摇一摇

---
+++ app/src/main/java/com/example/fds75/yaoyiyao/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.fds75.yaoyiyao;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private SensorManager sensorManager;
+    private Vibrator vibrator;
+
+    private static String strs[] = {"石头","剪刀","布"};
+    private static int pics[] = {R.mipmap.p1,R.mipmap.p2,R.mipmap.p3};
+
+    private static final int SENSOR_SHAKE = 10;
+    private TextView text;
+    private ImageView img;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.txtlabel);
+        img = (ImageView)findViewById(R.id.imageView);
+
+        //定义两个传感器
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+
+    }
+
+    protected void onResume(){
+        super.onResume();
+        if (sensorManager != null){//注册监听器
+            sensorManager.registerListener(sensorEventListener,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+            //第一个参数是Listener，第二个参数是所获得传感器类型，第三个参数值获得传感器信息的频率
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if (sensorManager != null){
+            sensorManager.unregisterListener(sensorEventListener);
+        }
+    }
+
+    //重力感应监听
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            //传感器信息改变时执行此方法
+            float[] values = event.values;
+            float x = values[0];//x轴方向的重力加速度，向右为正
+            float y = values[1];//y轴方向的重力加速度，向前为正
+            float z = values[2];//z轴方向的重力加速度，向上为正
+            Log.i(TAG, "x{"+x+ "]y["+y+"]z["+z+"]");
+            //一般在这三个方向的重力加速度达到40就达到了摇晃手机的状态
+            int medumValue = 10;//不同的手机厂商这个数值可能会不同
+            if (Math.abs(x) > medumValue || Math.abs(y) > medumValue || Math.abs(z) > medumValue){
+                vibrator.vibrate(200);
+                Message msg = new Message();
+                msg.what = SENSOR_SHAKE;
+                handler.sendMessage(msg);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 10:
+                    //Toast.makeText(ShackActicity.this,"检测到摇晃，执行操作！",Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "检测到摇晃，执行操作！");
+                    java.util.Random r = new java.util.Random();//构造随机数
+                    int num = Math.abs(r.nextInt())%3;
+                    text.setText(strs[num]);
+                    img.setImageResource(pics[num]);
+                    break;
+            }
+        }
+    };
+
+}
